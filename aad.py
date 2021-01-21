41a38d83f45c7e5fa0b5f45aa899c8a5a8ee5410
rluks <rluks101@gmail.com>
2021-01-21 22:47:50 +0100

    Grabbing works

--- aad.py
+++ aad.py
@@ -2,6 +2,9 @@ import csv
 import re
 import urllib.request
 import unicodedata
+import html
+from urllib.parse import urlparse
+import os
 
 filename = 'People-Raw.csv'
 
@@ -9,23 +12,35 @@ def strip_accents(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s)
                   if unicodedata.category(c) != 'Mn')
 
-#with open(filename, 'r', encoding="mbcs") as csvfile:
-with open(filename, 'r', encoding="utf-8") as csvfile:
-    datareader = csv.reader(csvfile)
-    count = 0
-    for row in datareader:
-        if(count == 0):
-            count += 1
-            continue
-        name = row[0]
-        urlrow = row[1]
-        if(name == "" or urlrow == ""):
-            continue
-        urlbrackets = re.search("\(https:\/\/dl.airtable.com/.*\)$", urlrow)
-        urlbracket = urlbrackets.group()[1:]
-        url = urlbracket[:-1]
-        nameascii2 = strip_accents(name)
-        print(nameascii2)
+def grab():
+    with open(filename, 'r', encoding="utf-8") as csvfile:
+        datareader = csv.reader(csvfile)
+        count = 0
+        for row in datareader:
 
+            if(count == 0):
+                count += 1
+                continue
 
-        #urllib.request.urlretrieve(url, file_name)
+            name = row[0]
+            urlrow = row[1]
+
+            if(name == "" or urlrow == ""):
+                continue
+
+            urlbrackets = re.search("\(https:\/\/dl.airtable.com/.*\)$", urlrow)
+            urlbracket = urlbrackets.group()[1:]
+            url = urlbracket[:-1]
+
+            nameascii = strip_accents(name)
+            nameesc = html.escape(nameascii)
+
+            path = urlparse(url).path
+            ext = os.path.splitext(path)[1]
+            
+            file_name = nameesc + ext
+
+            print("Grabbing " + file_name)
+            urllib.request.urlretrieve(url, file_name)
+
+grab()
